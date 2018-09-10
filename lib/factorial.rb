fe264190e7fb0535adc55e57aed3f390a5b49262
Alice Hsiao <alice.hsiao87@gmail.com>
2018-09-09 17:32:33 -0700

    Tested and implemented one version of factorial

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,20 @@
 # Computes factorial of the input number and returns it
+# Time Complexity: O(n) since it counts down and multiplies each number
+# Space Complexity: O(1) aside from one variable, there is no additional space taken
+require 'pry'
+
+# Consider Recursion
 def factorial(number)
-  raise NotImplementedError
+  if number == nil
+    raise ArgumentError.new("Not an integer")
+  end
+
+  answer = 1
+
+  while number >= 1
+    answer *= number
+    number -= 1
+  end
+
+  return answer
 end
