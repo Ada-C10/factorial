cd5c1272c85287ad8e46ff1ce256e1683c1a1956
Alice Hsiao <alice.hsiao87@gmail.com>
2018-09-09 17:33:02 -0700

    Edited comments above code

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,7 +1,6 @@
 # Computes factorial of the input number and returns it
 # Time Complexity: O(n) since it counts down and multiplies each number
 # Space Complexity: O(1) aside from one variable, there is no additional space taken
-require 'pry'
 
 # Consider Recursion
 def factorial(number)
