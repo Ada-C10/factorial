704300988cb62d90e31b107ffa5d946ebf1e2dd7
Daniela Di Iorio <danielad@gmail.com>
2018-09-12 22:06:21 -0700

    authored factorial

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,14 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError if number == nil
+  return 1 if number == 0
+
+  product = number
+  next_num = number
+
+  until next_num == 1
+    next_num -= 1
+    product *= next_num
+  end
+  return product
 end
