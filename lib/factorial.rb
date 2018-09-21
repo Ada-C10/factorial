fdbea56fbc477da9c6f78bf0e3d2969eb071e877
Jane <jane.edwards.mcnear@gmail.com>
2018-09-21 14:41:22 -0700

    Completed Factorial

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,15 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+    raise ArgumentError if number.nil?
+
+    i = 0
+    j = number
+    factorial = 1
+    while i < j
+      factorial *= number
+      number -= 1
+      i += 1
+    end
+
+    return factorial
 end
