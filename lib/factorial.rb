8798a42701d65ac90b19317391f92f33b1077aa4
Jazz <jazziesf21@yahoo.com>
2018-09-24 22:23:29 -0700

    Jazz Factorial

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,25 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+
+  raise ArgumentError if number == nil
+
+  count = (number -1)
+  sum = count * number
+  if number == 1 || number == 0
+    sum = 1
+    return sum
+  end
+
+
+  while count > 0
+    count -= 1
+    if count == 1
+      sum = sum * count
+    elsif count > 1
+      sum = sum * count
+    end
+  end
+
+  return sum
+
 end
