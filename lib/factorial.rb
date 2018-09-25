2020e8d94432f6cceda2b75b22c5782bb0dfb110
Trang <trang.frego@gmail.com>
2018-09-25 11:56:48 -0700

    create method and pass tests

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,13 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError if number == nil
+
+  result = 1
+
+  while number > 0
+    result = result * number
+    number -= 1
+  end
+
+  return result
 end
