d4378a0dee500a864bfcd5367b6b85b82fa4d676
Divya <divyamohanan@gmail.com>
2018-09-26 15:13:31 -0700

    Factorial - Divya

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,10 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  index = 0
+  factorial = 1
+  while index < number
+    factorial *= (number - index)
+    index += 1
+  end
+  return factorial
 end
