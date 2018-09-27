6f1078a564abfd8d1df5b038fa659bf56ac2ebd9
Divya <divyamohanan@gmail.com>
2018-09-26 22:18:56 -0700

    Time and space complexity

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,7 @@
 # Computes factorial of the input number and returns it
-Time complexity is CONSTANT.
-Space complexity is CONSTANT
+# Time complexity is O(n) as the loop is running n times
+# Space complexity is CONSTANT
+
 def factorial(number)
   index = 0
   factorial = 1
