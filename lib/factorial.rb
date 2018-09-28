33f59aa4dc440f94b00cd98a492534ebace3e7d8
Anibel America <ameriabel@gmail.com>
2018-09-28 16:02:17 -0700

    adds first implementation of factorial algorithm, uses recursion

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,10 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  if number == nil
+    raise ArgumentError
+  elsif number == 0 || number == 1
+    return 1
+  else
+    return number * factorial(number - 1)
+  end
 end
