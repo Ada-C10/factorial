357964145cff731d6ee2789a4224a72aee9e78e6
Hannah <hannahvanseeters@gmail.com>
2018-09-27 21:38:58 -0700

    complete with tests passing

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,13 @@
 # Computes factorial of the input number and returns it
+
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError, "Invalid Number" if number.nil?
+
+  total = 1
+
+  number.times do
+    total *= number
+    number -= 1
+  end
+  return total
 end
