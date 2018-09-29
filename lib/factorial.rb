9ec39f6f915d48d4a2c13aaf1d6f8aea8574f997
Anibel America <ameriabel@gmail.com>
2018-09-28 18:21:21 -0700

    iterative implementation of factorial lagorithm

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -2,9 +2,18 @@
 def factorial(number)
   if number == nil
     raise ArgumentError
-  elsif number == 0 || number == 1
-    return 1
-  else
-    return number * factorial(number - 1)
   end
+
+  if number == 0
+    return 1
+  end
+
+  answer = number
+
+  while number > 1
+    answer *= number - 1
+    number -= 1
+  end
+
+  return answer
 end
