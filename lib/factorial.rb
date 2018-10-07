7163fad5a4c79ee17b6da21f62d5f72dd16db5ac
Addison Cole <addison.cole85@gmail.com>
2018-10-06 17:22:02 -0700

    add method to calculate a factorial, given a positive integer.

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,16 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  if number == nil
+    return raise ArgumentError
+  end
+  if number == 0
+    return 1
+  end
+  a = number
+  b = a
+  until a == 1 do
+    a -= 1
+    b *= a
+  end
+  return b
 end
