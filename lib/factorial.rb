9afc24aae22373a645bf9fc09c792a39ea1efedd
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-07 18:58:39 -0700

    all tests passed

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,14 @@
 # Computes factorial of the input number and returns it
+require 'pry'
 def factorial(number)
-  raise NotImplementedError
+  sum = 1
+  if number == nil
+    raise ArgumentError.new("can't be nil")
+  end
+
+  while number > 0
+    sum = sum * (number)
+    number = number - 1
+  end
+  return sum
 end
