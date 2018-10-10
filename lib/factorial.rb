272a46e5fc3bac58a72c7190962da85d5da4ddba
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-09 20:11:07 -0700

    Da tests r paassin

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,18 @@
 # Computes factorial of the input number and returns it
+
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError if number.nil?
+  raise ArgumentError unless ( number.class == Integer && number >= 0 )
+
+  factorial = 1
+
+  number.times do
+    factorial *= number
+    number -= 1
+  end
+
+  return factorial
 end
+
+# Space complexity: O(1) constant
+# Time complexity: O(n) linear
