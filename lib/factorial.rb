db9dbb989600d54ef97f3a8cd72a1939a987ac16
Katricia Smith <smithkatricia@gmail.com>
2018-11-11 14:02:03 -0800

    Method for to return factorial of a number. Time + space complexity notes

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,20 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError if number == nil
+
+  return 1 if number == 0 || number == 1
+
+  multiplier = number
+  factorial = number
+
+  until multiplier == 1
+    # 6 * 5 * 4 * 3 * 2 * 1
+    factorial *= (multiplier - 1)
+    multiplier -= 1
+  end
+
+  return factorial
 end
+
+# Time complexity is O(n) where n is the number input, because the function must repeat multiplication as many times as "number"
+# Space complexity is O(1) because only two additional variables are created
