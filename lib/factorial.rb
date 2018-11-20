2ae54113187214d7b0e85d452653a1e8ffa3ec05
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-11-20 11:32:13 -0800

    fib brute force

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,23 @@
 # Computes factorial of the input number and returns it
-def factorial(number)
-  raise NotImplementedError
+def factorial(num)
+  if num.nil?
+    raise ArgumentError
+  end
+
+  if num == 0
+    return 1
+  else
+    x = num
+    fact = num
+    while x >= 2
+      fact *= (x - 1)
+      x -= 1
+    end
+
+    return fact
+  end
+
 end
+
+# y = factorial(6)
+# puts y
