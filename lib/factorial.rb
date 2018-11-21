a2ef46a16302ad28a0953882c473a9067ea52420
Alice Hsiao <alice.hsiao87@gmail.com>
2018-11-21 15:27:54 -0800

    Uncommented out my code

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -6,17 +6,17 @@ def factorial(number)
   if number == nil
     raise ArgumentError.new("Not an integer")
   end
-  #
-  # answer = 1
-  #
-  # while number >= 1
-  #   answer *= number
-  #   number -= 1
-  # end
-  #
-  # return answer
+  
+  answer = 1
+
+  while number >= 1
+    answer *= number
+    number -= 1
+  end
+
+  return answer
 
   # Recursion
-  return 1 if number == 1 || number == 0
-  return number * factorial(number - 1)
+  # return 1 if number == 1 || number == 0
+  # return number * factorial(number - 1)
 end
