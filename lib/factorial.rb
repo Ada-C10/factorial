9d884c259b9eb18b8f41bbf3151bcb23d371bba2
Dionisia Montanez <dmontanez15@gmail.com>
2018-11-23 22:21:54 -0800

    factorial with passing tests

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,15 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  # raise NotImplementedError
+  raise ArgumentError if number == nil
+
+  # Starting point will be 1. If the number is less than 1, then f will just return 1 and not run the while loop since 0! = 1.
+  f = 1
+
+  while number > 1
+    f = f * number
+    # decrements number until it reaches 1
+    number -= 1
+  end
+  return f
 end
